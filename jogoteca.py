606db80e8243e8f7ae5f42d2a73b0ff3142500bf
izabelrodrigues <izabel.castro.rodrigues@gmail.com>
2019-02-17 00:18:15 -0300

    Adding more informations for the games

--- jogoteca.py
+++ jogoteca.py
@@ -1,7 +1,18 @@
 from flask import Flask,render_template
 app = Flask(__name__)
 
+class Game:
+    def __init__(self, name, company):
+        self.name = name
+        self.company = company
+
+def getGames():
+    game1 = Game('March of Empires', 'Gameloft')
+    game2 = Game('Canddy Crush', 'King')
+    list = [game1, game2]
+    return list
+
 @app.route("/")
 def lista():
-    lista = ['Tetris','March of Empires', 'Candy Crush']
+    lista = getGames()
     return render_template('lista.html', title='Jogos', games=lista)
