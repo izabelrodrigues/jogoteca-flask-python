960aa33f71ec4d98ef7f2a93b8ab864b3b581d88
izabelrodrigues <izabel.castro.rodrigues@gmail.com>
2019-02-17 12:01:04 -0300

    Adding form and route for create a new game

--- jogoteca.py
+++ jogoteca.py
@@ -1,4 +1,4 @@
-from flask import Flask,render_template
+from flask import Flask,render_template, request
 from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
@@ -16,6 +16,21 @@ def getGames():
     return list
 
 @app.route("/")
-def lista():
+def index():
     lista = getGames()
+    return render_template('lista.html', title='  Jogos  ', games=lista)
+
+@app.route("/new-game")
+def novo():
+    return render_template('novo.html', title='Novo Jogo')
+
+@app.route('/add-game', methods=['POST',])
+def create():
+    lista = getGames()
+    name = request.form['nome']
+    company = request.form['empresa']
+    
+    game = Game(name,company)
+    lista.append(game)
+
     return render_template('lista.html', title='  Jogos  ', games=lista)
