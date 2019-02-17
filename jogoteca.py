6473484535bba9a2c6daa97d31c91bd1d0f7ab35
izabelrodrigues <izabel.castro.rodrigues@gmail.com>
2019-02-16 23:53:23 -0300

    Sending the games to the html template by parameters

--- jogoteca.py
+++ jogoteca.py
@@ -2,5 +2,6 @@ from flask import Flask,render_template
 app = Flask(__name__)
 
 @app.route("/")
-def hello():
-    return render_template('lista.html')
+def lista():
+    lista = ['Tetris','March of Empires', 'Candy Crush']
+    return render_template('lista.html', title='Jogos', games=lista)
