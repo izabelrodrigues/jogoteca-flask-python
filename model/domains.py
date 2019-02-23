8651362d34be476cc102fc7114becbc53cfc588e
izabelrodrigues <izabel.castro.rodrigues@gmail.com>
2019-02-23 20:15:37 -0300

    refactoring:  removing classes definition to domains.py file

---
+++ model/domains.py
@@ -0,0 +1,10 @@
+class Game:
+    def __init__(self, name, company):
+        self.name = name
+        self.company = company
+
+class Usuario:
+    def __init__(self, id, nome, senha):
+        self.id = id
+        self.nome = nome
+        self.senha = senha
