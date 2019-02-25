1eca3484ea3299dee1fba16498f17995cc289678
izabelrodrigues <izabel.castro.rodrigues@gmail.com>
2019-02-25 01:15:30 -0300

    improving the game's image logic

--- jogoteca.py
+++ jogoteca.py
@@ -2,7 +2,7 @@ from flask import Flask,render_template, request, redirect, session, flash, url_
 from domains import Game, Usuario
 from flask_mysqldb import MySQLdb
 from dao import JogoDao, UsuarioDao
-import os, sys
+import os, sys, time
 
 app = Flask(__name__)
 app.secret_key = 'alura'
@@ -36,7 +36,8 @@ def create():
 
     arquivo = request.files['arquivo']
     upload_path = app.config['UPLOAD_PATH']
-    arquivo.save(f'{upload_path}/capa{game.id}.jpg')
+    timestamp = time.time()
+    arquivo.save(f'{upload_path}/capa{game.id}-{timestamp}.jpg')
 
     return redirect(url_for('index'))
 
@@ -45,7 +46,8 @@ def edit(id):
     if 'usuario_logado' not in session or session['usuario_logado'] == None:
         return redirect(url_for('login', proxima=url_for('edit', id=id)))
     jogo = jogo_dao.busca_por_id(id)
-    return render_template('games/edit.html', title='Editar Jogo', game = jogo, capa_jogo=f'capa{id}.jpg')
+    nome_imagem = findImagem(id)
+    return render_template('games/edit.html', title='Editar Jogo', game = jogo, capa_jogo=nome_imagem or 'capa_padrao.jpg')
 
 @app.route('/update/<int:id>', methods=['POST',])
 def update(id):
@@ -53,6 +55,12 @@ def update(id):
     categoria = request.form['categoria']
     console = request.form['console']
     game = Game(nome,categoria,console, id)
+    arquivo = request.files['arquivo']
+    deleteImage(game.id)
+
+    upload_path = app.config['UPLOAD_PATH']
+    timestamp = time.time()
+    arquivo.save(f'{upload_path}/capa{game.id}-{timestamp}.jpg')
     jogo_dao.salvar(game)
 
     return redirect(url_for('index'))
@@ -93,3 +101,12 @@ def logout():
 @app.route('/uploads/<filename>')
 def imagem(filename):
     return send_from_directory('uploads',filename)
+
+def findImagem(id):
+    for nome_arquivo in os.listdir(app.config['UPLOAD_PATH']):
+        if f'capa{id}' in nome_arquivo:
+            return nome_arquivo
+
+def deleteImage(id):
+    arquivo = findImagem(id)
+    os.remove(os.path.join(app.config['UPLOAD_PATH'], arquivo))
